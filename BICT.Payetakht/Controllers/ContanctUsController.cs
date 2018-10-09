8f5e1953e375a948a1039d9d99737b8533ba520b
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-09 16:25:47 +0330

    paging07/17

---
+++ BICT.Payetakht/Controllers/ContanctUsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BICT.Payetakht.Controllers
+{
+    public class ContanctUsController : Controller
+    {
+        // GET: ContancUs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
