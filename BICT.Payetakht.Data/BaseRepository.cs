f27f0c4cd78be61fc93e174ca0fb52ce8b6ca474
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-10 17:26:36 +0330

    Responsitory

---
+++ BICT.Payetakht.Data/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BICT.Payetakht.Data
+{
+    public class BaseRepository : IDisposable
+    {
+        protected BICTDbContext db;
+
+        public BaseRepository()
+        {
+            db = new BICTDbContext();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
