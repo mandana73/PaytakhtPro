facdc0a59c6e2234b0e53d1fe91c18ae07815096
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-10 17:26:20 +0330

    Controller

--- BICT.Payetakht/Areas/Dashboard/Controllers/CarManufactureController.cs
+++ BICT.Payetakht/Areas/Dashboard/Controllers/CarManufactureController.cs
@@ -1,17 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using BICT.Payetakht.Data.Repository;
+using BICT.Payetakht.Data.ViewModels;
 
 namespace BICT.Payetakht.Areas.Dashboard.Controllers
 {
     public class CarManufactureController : Controller
     {
-        // GET: Dashboard/CarManufacture
+        private CarManufactureRepository repository;
+
+        public CarManufactureController()
+        {
+            repository = new CarManufactureRepository();
+        }
+
         public ActionResult Index()
+        {
+            var list = repository.GetList();
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(CarManufactureViewModel model)
+        {
+            repository.Create(model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+           var b= repository.GetItem(ID);
+
+            return View(b);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int ID,CarManufactureViewModel carManufactureViewModel)
+        {
+            repository.Edit(carManufactureViewModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public ActionResult Delet(int ID)
+        {
+            repository.Delet(ID);
+            return RedirectToAction(nameof(Index));
+        }
+        
+
     }
 }
