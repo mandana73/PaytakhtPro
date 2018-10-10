688d31702c7d22edabc601a303ea252bff29fafc
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-10 12:02:38 +0330

    DashboardChange

--- BICT.Payetakht/Areas/Dashboard/DashboardAreaRegistration.cs
+++ BICT.Payetakht/Areas/Dashboard/DashboardAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace BICT.Payetakht.Areas.Dashboard
             context.MapRoute(
                 "Dashboard_default",
                 "Dashboard/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
