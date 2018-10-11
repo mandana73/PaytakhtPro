eb377ad0ed6cf0c50d9e19e5b2b805c6f382c57d
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-11 13:32:56 +0330

    CarYearResponsitory.cs

---
+++ BICT.Payetakht.Data/Repository/CarYearResponsitory.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BICT.Payetakht.Data.Models;
+using BICT.Payetakht.Data.ViewModels;
+
+
+namespace BICT.Payetakht.Data.Repository
+{
+    public class CarYearRepository : BaseRepository
+    {
+        public IList<CarYearViewModel> GetList()
+        {
+            return db.CarYears
+                     .Select(x => new CarYearViewModel
+                     {
+                         ID = x.ID,
+                        Year = x.Year
+                     })
+                     .ToList();
+        }
+        public CarYearViewModel GetItem(int id)
+        {
+            return db.CarYears.Where(x => x.ID == id)
+                .Select(x => new CarYearViewModel
+                {
+                    ID = x.ID,
+                    Year = x.Year,
+
+                }).FirstOrDefault();
+        }
+
+        public void Create(CarYearViewModel CarYear)
+        {
+            var item = new CarYear { Year = CarYear.Year };
+            db.CarYears.Add(item);
+            db.SaveChanges();
+
+        }
+        public void Edit(CarYearViewModel carYearView)
+        {
+            CarYear a = db.CarYears.Find(carYearView.ID);
+            a.Year = carYearView.Year;
+            db.SaveChanges();
+        }
+        public void Delete(int ID)
+        {
+            var a = db.CarYears.Find(ID);
+            db.CarYears.Remove(a);
+            db.SaveChanges();
+        }
+
+    }
+}
