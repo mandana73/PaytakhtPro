3c5c67d690070f6ec5bd7b45e91e956f2da55344
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-11 13:33:21 +0330

    CarModelViewModel.cs

---
+++ BICT.Payetakht.Data/ViewModels/CarYearsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+    public class CarYearViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "عنوان را وارد نمایید")]
+        [StringLength(255)]
+        [Display(Name = "عنوان سازنده")]
+        public int Year { get; set; }
+    }
+}
