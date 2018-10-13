2d5263914b7e77f783136f6d78d0afc8196ab011
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-13 17:00:06 +0330

    CarDetail.cs

---
+++ BICT.Payetakht.Data/Models/CarDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace BICT.Payetakht.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CarDetail
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int CarModelID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public virtual CarModel CarModel { get; set; }
+    }
+}
