59f114567226f8066f1a41b3604e4efb7c6297b7
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-13 16:59:26 +0330

    CarDetailRespository.cs

---
+++ BICT.Payetakht.Data/Repository/CarDetailRespository.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BICT.Payetakht.Data.Models;
+using BICT.Payetakht.Data.ViewModels;
+
+namespace BICT.Payetakht.Data.Repository
+{
+    public class CarDetailRepository : BaseRepository
+    {
+        public IList<CarDetailViewModel> GetList()
+        {
+            return db.CarDetails
+                     .Select(x => new CarDetailViewModel
+                     {
+                         ID = x.ID,
+                         CarModelTitle = x.CarModel.Title,
+                         Title = x.Title,
+                     }).ToList();
+        }
+
+        public CarDetailViewModel GetItem(int id)
+        {
+            return db.CarDetails.Where(x => x.ID == id)
+                .Select(x => new CarDetailViewModel
+                {
+                    ID = x.ID,
+                    Title = x.Title,
+                    CarModelID = x.CarModelID,
+                }).FirstOrDefault();
+        }
+
+        public void Create(CarDetailViewModel carDetail)
+        {
+            var item = new CarDetail()
+            {
+                Title = carDetail.Title,
+                CarModelID = carDetail.CarModelID,
+            };
+            db.CarDetails.Add(item);
+            db.SaveChanges();
+        }
+
+        public void Edit(CarDetailViewModel carDetailView)
+        {
+            CarDetail carDetail = db.CarDetails.Find(carDetailView.ID);
+            carDetail.Title = carDetailView.Title;
+            db.SaveChanges();
+        }
+
+        public void Delete(int ID)
+        {
+            var carDetail = db.CarDetails.Find(ID);
+            db.CarDetails.Remove(carDetail);
+            db.SaveChanges();
+        }
+    }
+}
