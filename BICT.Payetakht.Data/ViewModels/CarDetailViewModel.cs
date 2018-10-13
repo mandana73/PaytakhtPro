39826693e60c1747364f467c06db7afb773fe250
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-13 16:58:51 +0330

    CarDetailViewModel.cs

---
+++ BICT.Payetakht.Data/ViewModels/CarDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+    public class CarDetailViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "جزئیات را وارد نمایید")]
+        [Display(Name = "جزئیات")]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public int CarModelID { get; set; }
+        public string CarModelTitle { get; set; }
+    }
+}
