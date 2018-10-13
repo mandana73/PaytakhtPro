c033fe0c3fe8596dd3ec759fc5259c6feec07155
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-13 16:56:56 +0330

    CarYear

--- BICT.Payetakht.Data/Models/CarYear.cs
+++ BICT.Payetakht.Data/Models/CarYear.cs
@@ -1,15 +1,9 @@
 ﻿namespace BICT.Payetakht.Data.Models
 {
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
     public class CarYear
     {
-        public CarYear()
-        {
-            CarDetails = new HashSet<CarDetail>();
-        }
-
         [Key]
         public int ID { get; set; }
 
@@ -17,9 +11,6 @@
 
         public int Year { get; set; }
 
-        public virtual CarModels CarModels { get; set; }
-
-        public ICollection<CarDetail> CarDetails { get; set; }
-        public CarModels CarModel { get; internal set; }
+        public virtual CarModel CarModel { get; set; }
     }
 }
