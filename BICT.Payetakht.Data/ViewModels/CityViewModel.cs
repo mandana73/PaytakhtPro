1064d56c3626561240b40c526ab39a57bb2ae7a8
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-14 15:10:27 +0330

    CityViewModel.cs

---
+++ BICT.Payetakht.Data/ViewModels/CityViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+   public class CityViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "شهر راه انتخاب کنید")]
+        [StringLength(255)]
+        [Display(Name = "انتخاب استان")]
+        public string Title { get; set; }
+    }
+}
