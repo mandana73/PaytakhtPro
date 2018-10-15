d548cc91b0823ce897e6be2c0b4dea1a3f51bce9
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-15 16:25:22 +0330

    CarModelYearDetailViewModel.cs

---
+++ BICT.Payetakht.Data/ViewModels/CarModelYearDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+   public class CarModelYearDetailViewModel
+    {
+        public int ID { get; set; }
+
+       
+
+        public int CarModelID { get; set; }
+        public String CarModelTitle { get; set; }
+        public int CarDetailID { get; set; }
+        public String CarDetailTitle { get; set; }
+        public int CarYearID { get; set; }
+        public int CarYearTitle { get; set; }
+    }
+}
