0e21e4bcf130899d4e18fc50ebde07af09d30367
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-15 16:51:14 +0330

    CarModelYearDetailController.cs

--- BICT.Payetakht/Areas/Dashboard/Controllers/CarModelYearDetailController.cs
+++ BICT.Payetakht/Areas/Dashboard/Controllers/CarModelYearDetailController.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using BICT.Payetakht.Data.Repository;
 
 namespace BICT.Payetakht.Areas.Dashboard.Controllers
 {
     public class CarModelYearDetailController : Controller
     {
-        
+        private CarModelYearDetailRepository repository;
+
+        public CarModelYearDetailController()
+        {
+            repository = new CarModelYearDetailRepository();
+        }
 
         public ActionResult Index()
         {
-            return View();
+            var list = repository.GetList();
+            return View(list);
         }
     }
 }
