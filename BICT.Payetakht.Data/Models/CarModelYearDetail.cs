83be512aca6449ec22bf5a68337a9c18d0b7e08c
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-15 16:24:21 +0330

    CarModelYearDetail.cs

---
+++ BICT.Payetakht.Data/Models/CarModelYearDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BICT.Payetakht.Data.Models
+{
+    public class CarModelYearDetail
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int CarModelID { get; set; }
+        public int CarYearID { get; set; }
+        public int CarDetailID { get; set; }
+
+        public virtual CarModel CarModel { get; set; }
+        public virtual CarYear CarYear { get; set; }
+        public virtual CarDetail CarDetail { get; set; }
+        public object a { get; internal set; }
+    }
+}
