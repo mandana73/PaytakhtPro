1f0ab394aafd941437742aae70980a0041b3b943
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-15 16:25:42 +0330

    CarModelYearDetailController.cs

---
+++ BICT.Payetakht/Areas/Dashboard/Controllers/CarModelYearDetailController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BICT.Payetakht.Areas.Dashboard.Controllers
+{
+    public class CarModelYearDetailController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
