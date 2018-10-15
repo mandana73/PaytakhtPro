94a848bf7afe183fc54269351d10600a29153f7d
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-15 16:25:05 +0330

    CarModelYearDetailRepository.cs

---
+++ BICT.Payetakht.Data/Repository/CarModelYearDetailRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BICT.Payetakht.Data.Models;
+using BICT.Payetakht.Data.ViewModels;
+
+namespace BICT.Payetakht.Data.Repository
+{
+    public class CarModelYearDetailRepository : BaseRepository
+    {
+        public IList<CarModelYearDetailViewModel> GetList()
+        {
+            return db.CarModelYearDetails
+                .Select(x => new CarModelYearDetailViewModel
+                {
+                    ID = x.ID,
+                    CarDetailID = x.CarDetailID,
+                    CarDetailTitle = x.CarDetail.Title,
+                    CarModelID = x.CarModelID,
+                    CarModelTitle = x.CarModel.Title,
+                    CarYearID = x.CarYearID,
+                    CarYearTitle=x.CarYear.Year,
+
+
+                }).ToList();
+
+        }
+        public CarModelYearDetailViewModel GetItem(int id)
+        {
+            return db.CarModelYearDetails.Where(x => x.ID == id)
+                .Select(x => new CarModelYearDetailViewModel
+                {
+                    ID = x.ID,
+                    CarDetailID = x.CarDetailID,
+                    CarDetailTitle = x.CarDetail.Title,
+                    CarModelID = x.CarModelID,
+                    CarModelTitle = x.CarModel.Title,
+                    CarYearID = x.CarYearID,
+                    CarYearTitle=x.CarYear.Year,
+                }).FirstOrDefault();
+
+        }
+        public void Create(CarModelYearDetailViewModel carModelYearDetailView)
+        {
+            var item = new CarModelYearDetail
+            {
+                CarDetailID = carModelYearDetailView.CarDetailID,
+                CarModelID = carModelYearDetailView.CarModelID,
+                CarYearID = carModelYearDetailView.CarYearID,
+            };
+            db.CarModelYearDetails.Add(item);
+            db.SaveChanges();
+        }
+        public void Edit(CarModelYearDetailViewModel carModelYearDetailView)
+        {
+          
+            CarModelYearDetail a = db.CarModelYearDetails.Find(carModelYearDetailView.ID);
+            a.CarYearID = carModelYearDetailView.CarYearID;
+            a.CarModelID = carModelYearDetailView.CarModelID;
+            a.CarDetailID = carModelYearDetailView.CarDetailID;
+
+            db.SaveChanges();
+        }
+
+        public void Delete(int ID)
+        {
+            var a = db.CarModelYearDetails.Find(ID);
+            db.CarModelYearDetails.Remove(a);
+            db.SaveChanges();
+        }
+    }
+
+
+}
+
+
+
