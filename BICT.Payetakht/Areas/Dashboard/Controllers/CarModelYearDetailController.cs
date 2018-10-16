12e9b6e13508681861f7cb22e958da5b8e769667
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-16 16:20:50 +0330

    CarModelYearDetailController.cs

--- BICT.Payetakht/Areas/Dashboard/Controllers/CarModelYearDetailController.cs
+++ BICT.Payetakht/Areas/Dashboard/Controllers/CarModelYearDetailController.cs
@@ -1,15 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using BICT.Payetakht.Data.Repository;
+using BICT.Payetakht.Data.ViewModels;
 
 namespace BICT.Payetakht.Areas.Dashboard.Controllers
 {
     public class CarModelYearDetailController : Controller
     {
         private CarModelYearDetailRepository repository;
+        private CarModelRepository carModelRepository;
+        private CarYearRepository carYearRepository;
+        private CarDetailRepository carDetailRepository;
 
         public CarModelYearDetailController()
         {
             repository = new CarModelYearDetailRepository();
+            carModelRepository = new CarModelRepository();
+            carYearRepository = new CarYearRepository();
+            carDetailRepository = new CarDetailRepository();
         }
 
         public ActionResult Index()
@@ -17,5 +25,51 @@ namespace BICT.Payetakht.Areas.Dashboard.Controllers
             var list = repository.GetList();
             return View(list);
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var list = carModelRepository.GetList()
+                .Select(x => new SelectListItem
+                {
+                    Text = x.CarManufactureTitle + " - " + x.Title,
+                    Value = x.ID.ToString()
+                }).ToList();
+            list.Insert(0, new SelectListItem { Value = "", Text = "انتخاب نمایید" });
+            ViewBag.ModelList = list;
+            return View();
+        }
+
+        public JsonResult GetCarYear(int carModelID)
+        {
+            var list = carYearRepository.GetList(carModelID).Select(x => new SelectListItem
+            {
+                Text = x.Year.ToString(),
+                Value = x.ID.ToString()
+            }).ToList();
+            list.Insert(0, new SelectListItem { Value = "", Text = "انتخاب نمایید" });
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetCarDetail(int carModelID)
+        {
+            var list = carDetailRepository.GetList(carModelID)
+                .Select(x => new SelectListItem
+            {
+                Text = x.Title.ToString(),
+                Value = x.ID.ToString()
+            }).ToList();
+            list.Insert(0, new SelectListItem { Value = "", Text = "انتخاب نمایید" });
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Create(CarModelYearDetailViewModel car)
+        {
+            repository.Create(car);
+            return RedirectToAction("Index");
+        }
+
     }
 }
