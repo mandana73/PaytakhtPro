1d99ba2df8f2b6bc5747d0959934c9623c2f0a74
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-20 17:22:23 +0330

    Models-Audit

---
+++ BICT.Payetakht.Data/Models/Audit.cs
@@ -0,0 +1,27 @@
+﻿namespace BICT.Payetakht.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Audit
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int CarManufactureID { get; set; }
+        public int CarModelID { get; set; }
+        public int CarYearID { get; set; }
+        public int CarDetailID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Date { get; set; }
+
+        public virtual CarManufacturer CarManufacturer { get; set; }
+        public virtual CarModel CarModel { get; set; }
+        public virtual CarYear CarYear { get; set; }
+        public virtual CarDetail CarDetail { get; set; }
+
+        public int Price { get; set; }
+    }
+}
