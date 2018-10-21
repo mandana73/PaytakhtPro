97985a9c581833212251da454e3f9b1c315ddbe1
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-21 17:19:25 +0330

    Models-CarDetail.cs

--- BICT.Payetakht.Data/Models/CarDetail.cs
+++ BICT.Payetakht.Data/Models/CarDetail.cs
@@ -8,11 +8,12 @@
         public CarDetail()
         {
             CarModelYearDetails = new HashSet<CarModelYearDetail>();
+            Audits = new HashSet<Audit>();
         }
 
         [Key]
         public int ID { get; set; }
-
+        
         public int CarModelID { get; set; }
 
         [Required]
@@ -21,5 +22,6 @@
 
         public virtual CarModel CarModel { get; set; }
         public ICollection<CarModelYearDetail> CarModelYearDetails { get; set; }
+        public ICollection<Audit> Audits { get; set; }
     }
 }
