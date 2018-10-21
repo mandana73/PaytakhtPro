2ce917aaf5afa717659f9267691fc2ccfe26b87c
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-21 17:25:35 +0330

    AuditController.cs

---
+++ BICT.Payetakht/Areas/Dashboard/Controllers/AuditController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using BICT.Payetakht.Data.Repository;
+using BICT.Payetakht.Helper;
+
+namespace BICT.Payetakht.Areas.Dashboard.Controllers
+{
+    public class AuditController : Controller
+    {
+        private AuditRepository auditRepository;
+
+        public AuditController()
+        {
+            auditRepository = new AuditRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var audits = auditRepository.GetList();
+            return View(audits);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var audit = auditRepository.Getitem(id);
+            audit.RequestDatePersian = new PersianDateTime(audit.RequestDate).ToString();
+            return View(audit);
+        }
+
+        public ActionResult SetAsDone(int id)
+        {
+            var audit = auditRepository.Getitem(id);
+            if(audit ==null )
+            {
+                ViewBag.Error = "آیتم انتخاب نشده است";
+            return RedirectToAction(nameof (Index));
+            }
+            auditRepository.SetAsDone(id);
+            return RedirectToAction(nameof(Index));
+
+
+        }
+    }
+}
