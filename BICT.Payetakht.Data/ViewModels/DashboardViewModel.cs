653a81ae0970f9d09c5e6b9b9b2f5b23362616cb
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-22 11:42:23 +0330

    ViewModels/DashboardViewModel.cs

---
+++ BICT.Payetakht.Data/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+   public class DashboardViewModel
+    {
+        [Display(Name = "خوانده نشده")]
+        public int AuditUnRead { get; set; }
+
+        [Display(Name = "انجام نشده")]
+        public int AuditUnDone { get; set; }
+
+        [Display(Name = "تمامی درخواست ها")]
+        public int AuditAllRequest { get; set; }
+
+        [Display(Name ="تمامی سازنده ها")]
+        public int CarManufactureCount { get; set; }
+
+        [Display(Name = "تمامی مدل ها")]
+        public int CarModelCount { get; set; }
+
+        [Display(Name = "جزئیات")]
+        public int CarDetailCount { get; set; }
+
+
+    }
+}
