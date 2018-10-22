e3136d1d1505d665497f61158a014d0600c60832
Mohammad Amin Rashidi nejad <BICTClient01@bict.ir>
2018-10-22 17:39:03 +0330

    BICT.Payetakht Areas Dashboard CarModelYearDetailController Index paging++

--- BICT.Payetakht.Data/Repository/CityRepository.cs
+++ BICT.Payetakht.Data/Repository/CityRepository.cs
@@ -18,6 +18,24 @@ namespace BICT.Payetakht.Data.Repository
                 .ToList();
         }
 
+        public IList<CityViewModel> GetPagedList( int pageNum )
+        {
+            if (pageNum<1)
+            {
+                pageNum = 1;
+            }
+            var skip = (pageNum - 1) * 10;
+            return db.Cities
+                     .OrderByDescending(x=>x.ID)
+                     .Skip(skip)
+                     .Take(10)
+                     .Select(x => new CityViewModel
+                      {
+                          ID = x.ID,
+                          Title = x.Title,
+                     })
+                     .ToList();
+        }
         public CityViewModel GetItem(int id)
         {
             return db.Cities.Where(x => x.ID == id)
