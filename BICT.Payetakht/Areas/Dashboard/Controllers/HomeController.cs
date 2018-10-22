a069a70446447064728ea9591023cfb4ab7fca7a
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-22 11:42:35 +0330

    Areas/Dashboard/Controllers/HomeController.cs

--- BICT.Payetakht/Areas/Dashboard/Controllers/HomeController.cs
+++ BICT.Payetakht/Areas/Dashboard/Controllers/HomeController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BICT.Payetakht.Data.Repository;
+using BICT.Payetakht.Data.ViewModels;
 
 namespace BICT.Payetakht.Areas.Dashboard.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private CarManufactureRepository carManufactureRepository;
+        private CarModelRepository carModelRepository;
+        private AuditRepository auditRepository;
+        private CarDetailRepository carDetailRepository;
+        public HomeController()
+        {
+            auditRepository = new AuditRepository();
+            carManufactureRepository = new CarManufactureRepository();
+            carModelRepository = new CarModelRepository();
+            carDetailRepository = new CarDetailRepository();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var model = new DashboardViewModel();
+            model.AuditAllRequest = auditRepository.AllRequestCount();
+            model.AuditUnDone = auditRepository.GetUnDoneCount();
+            model.AuditUnRead = auditRepository.GetUnReadCount();
+            model.CarManufactureCount = carManufactureRepository.CarManufactureCount();
+            model.CarModelCount = carModelRepository.CarModelCount();
+            model.CarDetailCount = carDetailRepository.CarDetailCount();
+            return View(model);
         }
+
+
+       
     }
 }
