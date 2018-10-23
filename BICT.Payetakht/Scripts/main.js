710d7d5a8c60a1c5f6c61d5af2be443fb542f6f3
Raika <Raika.Maftoon@gmail.com>
2018-10-23 16:38:44 +0330

    update layout

--- BICT.Payetakht/Scripts/main.js
+++ BICT.Payetakht/Scripts/main.js
@@ -31,17 +31,6 @@ jQuery(document).ready(function ($) {
     $('.video-link').magnificPopup({
         type: 'iframe'
     });
-    //$(".hello_slid").slick({
-    //    dots: true,
-    //    infinite: false,
-    //    slidesToShow: 1,
-    //    slidesToScroll: 1,
-    //    arrows: true,
-    //    prevArrow: "<i class='glyphicon glyphicon-chevron-left nextprevleft'></i>",
-    //    nextArrow: "<i class='glyphicon glyphicon-chevron-right nextprevright'></i>",
-    //    autoplay: true,
-    //    autoplaySpeed: 2000
-    //});
     $(".audit_slid").slick({
         dots: true,
         infinite: false,
