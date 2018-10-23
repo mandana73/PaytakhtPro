b55ed2f3695e8dafb80d5a9d4e01eefc1d4fcbc5
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-23 16:30:59 +0330

    Global.asax.cs

--- BICT.Payetakht/Global.asax.cs
+++ BICT.Payetakht/Global.asax.cs
@@ -1,8 +1,11 @@
 ﻿namespace BICT.Payetakht
 {
+    using System.Data.Entity.Migrations;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using BICT.Payetakht.Data;
+    using BICT.Payetakht.Migrations;
 
     public class MvcApplication : System.Web.HttpApplication
     {
@@ -12,6 +15,18 @@
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            InitializeDatabase();
+        }
+
+        public void InitializeDatabase()
+        {
+            var context = new BICTDbContext();
+            if (!context.Database.Exists() || !context.Database.CompatibleWithModel(false))
+            {
+                var configuration = new Configuration();
+                var migrator = new DbMigrator(configuration);
+                migrator.Update();
+            }
         }
     }
 }
