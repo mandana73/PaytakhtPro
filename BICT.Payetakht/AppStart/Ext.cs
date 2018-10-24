ad687df5e62bda0a7bc9fda00112801c3b98de38
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-10-24 16:18:54 +0330

    appstar

---
+++ BICT.Payetakht/AppStart/Ext.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using BICT.Payetakht.Data.Repository;
+
+namespace BICT.Payetakht
+{
+    public static class Ext
+    {
+        public static string GetMetaKeyWord(this HtmlHelper html)
+        {
+            var repository = new MetaKeyWordRepository();
+            var list = repository.GetList().Select(x => x.Title.Trim()).ToList();
+            return string.Join(",", list);
+        }
+    }
+}
