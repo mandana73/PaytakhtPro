baf490b47b30da385cf75752948dbcfd54d98119
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-03 13:46:35 +0330

    چیدمان

---
+++ BICT.Payetakht/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BICT.Payetakht.Data.Repository;
+
+namespace BICT.Payetakht.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogRepository repository;
+        public BlogController()
+        {
+            repository = new BlogRepository();
+        }
+        public ActionResult Index(int p=1)
+        {
+            ViewBag.Page = p;
+            var list = repository.GetPagedList(p,5);
+            return View(list);
+        }
+        public ActionResult Post(int id)
+        {
+            var a = repository.GetItem(id);
+            return View (a);
+        }
+    }
+}
