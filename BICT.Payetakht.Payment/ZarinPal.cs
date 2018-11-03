d50fee1581b82b32d001562f41db107d669196c7
sooshians.h@gmail.com <sooshians.h@gmail.com>
2018-11-03 15:16:57 +0330

    update

---
+++ BICT.Payetakht.Payment/ZarinPal.cs
@@ -0,0 +1,37 @@
+﻿
+namespace BICT.Payetakht.Payment
+{
+    public class ZarinPal
+    {
+        public static int ZarinpalPayment(string MerchantID, int Amount, string Description, string Email, string Mobile, string CallbackURL, out string Authority)
+        {
+            try
+            {
+                ZarinPalService.PaymentGatewayImplementationServicePortTypeClient zp = new ZarinPalService.PaymentGatewayImplementationServicePortTypeClient();
+                var Status = zp.PaymentRequest(MerchantID, Amount, Description, Email, Mobile, CallbackURL, out Authority);
+                return Status;
+            }
+            catch
+            {
+                Authority = string.Empty;
+                return -1000;
+            }
+        }
+        public static int ZarinpalPaymentVerification(string MerchantID, string Authority, int Amount)
+        {
+            try
+            {
+                ZarinPalService.PaymentGatewayImplementationServicePortTypeClient zp = new ZarinPalService.PaymentGatewayImplementationServicePortTypeClient();
+
+                var Status = zp.PaymentVerification(MerchantID, Authority, Amount, out long RefID);
+                return Status;
+            }
+            catch
+            {
+                Authority = string.Empty;
+                return -1000;
+            }
+        }
+
+    }
+}
