2f39d221026905e67a148a2c64cb20f9ae3ededf
sooshians.h@gmail.com <sooshians.h@gmail.com>
2018-11-04 15:59:36 +0330

    update

--- BICT.Payetakht.Payment/ZarinPal.cs
+++ BICT.Payetakht.Payment/ZarinPal.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace BICT.Payetakht.Payment
 {
     public class ZarinPal
@@ -8,28 +10,62 @@ namespace BICT.Payetakht.Payment
             Authority = string.Empty;
             try
             {
-                ZarinPalService.PaymentGatewayImplementationServicePortTypeClient zp = new ZarinPalService.PaymentGatewayImplementationServicePortTypeClient();
-                var Status = zp.PaymentRequest(MerchantID, Amount, Description, Email, Mobile, CallbackURL, out Authority);
+                ZarinPalService.PaymentGatewayImplementationServicePortTypeClient client = new ZarinPalService.PaymentGatewayImplementationServicePortTypeClient();
+                var Status = client.PaymentRequest(MerchantID, Amount, Description, Email, Mobile, CallbackURL, out Authority);
                 return Status;
             }
-            catch
+            catch ()
             {
                 return -1000;
             }
         }
-        public static int ZarinpalPaymentVerification(string MerchantID, string Authority, int Amount)
+        public static int ZarinpalPaymentVerification(string MerchantID, string Authority, int Amount, out long RefID)
         {
-            Authority = string.Empty;
-            try
-            {
-                ZarinPalService.PaymentGatewayImplementationServicePortTypeClient zp = new ZarinPalService.PaymentGatewayImplementationServicePortTypeClient();
 
-                var Status = zp.PaymentVerification(MerchantID, Authority, Amount, out long RefID);
-                return Status;
-            }
-            catch
+            ZarinPalService.PaymentGatewayImplementationServicePortTypeClient zp = new ZarinPalService.PaymentGatewayImplementationServicePortTypeClient();
+            var Status = zp.PaymentVerification(MerchantID, Authority, Amount, out RefID);
+            return Status;
+
+
+        }
+        public static string GetMessage(int status)
+        {
+            switch (status)
             {
-                return -1000;
+                case -1:
+                    return "اطلاعات ارسال شده  ناقص است.";
+                case -2:
+                    return "IP و یا مرچنت کد پذیرنده صحیح نیست.";
+                case -3:
+                    return "با توجه به محدودیت های شاپرک امکان پرداخت با رقم درخواست شده میسر نمی باشد.";
+                case -4:
+                    return "سطح تایید پذیرنده پایین تر از سطح نقره ای است.";
+                case -11:
+                    return "درخواست مورد نظر یافت نشد.";
+                case -12:
+                    return "امکان ویرایش درخواست میسر نمی باشد.";
+                case -21:
+                    return "هیچ نوع عملیات مالی برای این تراکنش یافت نشد.";
+                case -22:
+                    return "تراکنش ناموفق می باشد.";
+                case -33:
+                    return "رقم تراکنش با رقم پرداخت شده مطابقت ندارد.";
+                case -34:
+                    return "سقف تقسیم تراکنش از لحاظ تعداد یا رقم عبور نموده است.";
+                case -40:
+                    return "اجازه دسترسی به متد مربوطه وجود ندارد.";
+                case -41:
+                    return "اطلاعات ارسال شده مربوط به AdditionalData غیر معتبر می باشد.";
+                case -42:
+                    return "مدت زمان معتبر طول عمر شناسه پرداخت باید بین 30 دقیقه تا 45 روز می باشد.";
+                case -54:
+                    return "درخواست مورد نظر آرشیو شده است.";
+                case 100:
+                    return "عملیات با موفقیت انجام گردیده است.";
+                case 101:
+                    return "عملیات پرداخت موفق بوده و قبلاً PaymentVerification تراکنش انجام شده است.";
+                default:
+                    return "کد تعریف نشده.";
             }
         }
 
--- BICT.Payetakht/Controllers/AuditController.cs
+++ BICT.Payetakht/Controllers/AuditController.cs
@@ -100,12 +100,57 @@
         {
             var requestitem = auditTempRepository.GetItem(ID);
             string MerchantID = "e53f3f7c-d9f1-11e8-a28f-000c295eb8fc";
+            string authority;
             var urlWebConfig = ConfigurationManager.AppSettings["ZarinPalPayment"];
-            string CallbackURL = "http://" + Request.Url.Authority + urlWebConfig ;
-            var Payment = ZarinPal.ZarinpalPayment(MerchantID, requestitem.Price, "" , requestitem.Email,requestitem.Phone,CallbackURL, out string Authority);
+            string CallbackURL = "http://" + Request.Url.Authority + urlWebConfig + requestitem.ID;
+            int status = ZarinPal.ZarinpalPayment(MerchantID, requestitem.Price, "کارشناسی برای" + requestitem.CarModelTitle + " " + requestitem.CarManufactureTitle, requestitem.Email, requestitem.Phone, CallbackURL, out authority);
+            if (status == 100)
+            {
+                ////For release mode
+                Response.Redirect("https://zarinpal.com/pg/StartPay/" + authority);
+
+                ////For test mode
+                //Response.Redirect("https://sandbox.zarinpal.com/pg/StartPay/" + authority);
+                return null;
+            }
+            TempData["authority"] = authority;
+            TempData["Message"] = ZarinPal.GetMessage(status);
+            return View("Detail", requestitem);
+        }
+        public ActionResult ZarinpalPaymentVerification(int id)
+        {
+            if (!string.IsNullOrEmpty(Request.QueryString["Status"]) && !string.IsNullOrEmpty(Request.QueryString["Authority"]))
+            {
+                if (Request.QueryString["Status"].Equals("OK"))
+                {
+                    var requestitem = auditTempRepository.GetItem(id);
+                    long refID;
+                    int status = ZarinPal.ZarinpalPaymentVerification("MerchantID", Request.QueryString["Authority"], requestitem.Price, out refID);
+                    if (status == 100 || status == 101)
+                    {
+                        ViewBag.RefId = "کد پیگیری: " + refID + " - کد سفارش: " + id;
+                        auditTempRepository.Edit(id, refID, TempData["authority"].ToString());
+                        var audit = auditTempRepository.GetItem(id);
+                        audit.RequestDatePersian = new PersianDateTime(audit.RequestDate).ToString(PersianDateTimeFormat.Date);
+                        auditRepository.Create(audit);
+                        TempData["SuccessAudit"] = "Success";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.Message = ZarinPal.GetMessage(status);
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "کد مرجع: " + Request.QueryString["Authority"] + " - وضعیت:" + Request.QueryString["Status"];
+                }
+            }
+            else
+            {
+                ViewBag.Message = "ورودی نامعتبر است.";
+            }
             return View();
-
-
         }
 
         public JsonResult GetCarModel(int carManufactureID)
