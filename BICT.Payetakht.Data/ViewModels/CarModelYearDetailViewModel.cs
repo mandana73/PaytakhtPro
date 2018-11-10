9d53b847d8e43b4e8ee2a8f3c8481c0bbc9a5f19
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-10 14:21:56 +0330

    ارور مسیج ها

--- BICT.Payetakht.Data/ViewModels/CarModelYearDetailViewModel.cs
+++ BICT.Payetakht.Data/ViewModels/CarModelYearDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BICT.Payetakht.Data.ViewModels
 {
@@ -6,13 +7,20 @@ namespace BICT.Payetakht.Data.ViewModels
     {
         public int ID { get; set; }
 
+        [Required(ErrorMessage = "مدل را وارد نمایید")]
         public int CarModelID { get; set; }
         public String CarModelTitle { get; set; }
+
+        [Required(ErrorMessage = "جزئیات را وارد نمایید")]
         public int CarDetailID { get; set; }
         public String CarDetailTitle { get; set; }
+
+
+        [Required(ErrorMessage = "سال را وارد نمایید")]
         public int CarYearID { get; set; }
         public int CarYearTitle { get; set; }
 
+        [Required(ErrorMessage = "قیمت را وارد نمایید")]
         public int Price { get; set; }
     }
 }
