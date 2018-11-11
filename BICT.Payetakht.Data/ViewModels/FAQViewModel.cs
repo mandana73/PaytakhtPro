d8edabc49f060531a6342f544f5f197ee739aa23
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-11 14:35:17 +0330

    ویو مدل پرسش و پاسخ

---
+++ BICT.Payetakht.Data/ViewModels/FAQViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+   public class FAQViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "پرسش خود را مطرح کنید")]
+        [StringLength(1000)]
+        [Display(Name = "پرسش")]
+        public string Question { get; set; }
+
+        [AllowHtml]
+        [Required(ErrorMessage ="پاسخ را وارد نمایید")]
+        [StringLength(4000)]
+        [Display(Name = "پاسخ")]
+        public string Answer { get; set; }
+    }
+}
