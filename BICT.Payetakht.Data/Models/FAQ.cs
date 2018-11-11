339e57d553b9a7ba66ea0f6ca737804143ef72fd
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-11 14:48:57 +0330

    مدل پرسش پاسخ

---
+++ BICT.Payetakht.Data/Models/FAQ.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICT.Payetakht.Data.Models
+{
+   public class FAQ
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Question { get; set; }
+
+        [Required]
+        [StringLength(4000)]
+        public string Answer
+        { get; set; }
+    }
+}
