6ee344e1ed04967821f6f702e62a1f2b696c3a02
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-11 14:42:41 +0330

    migration

---
+++ BICT.Payetakht/Migrations/201811110911368_A019.cs
@@ -0,0 +1,16 @@
+namespace BICT.Payetakht.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class A019 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
