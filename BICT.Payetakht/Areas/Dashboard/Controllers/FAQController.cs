835310168be575b78047ede4351ec0c12cd1ce5d
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-11 14:49:54 +0330

    پرسش و پاسخ

---
+++ BICT.Payetakht/Areas/Dashboard/Controllers/FAQController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Mvc;
+using BICT.Payetakht.Data.Repository;
+using BICT.Payetakht.Data.ViewModels;
+
+namespace BICT.Payetakht.Areas.Dashboard.Controllers
+{
+    public class FAQController : Controller
+    {
+        private FAQRepository repository;
+
+        public FAQController()
+        {
+            repository = new FAQRepository();
+        }
+
+        public ActionResult Index(int p = 1)
+        {
+            ViewBag.Page = p;
+            var list = repository.GetPagedList(p, 10);
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FAQViewModel fAQ)
+        {
+            repository.Create(fAQ);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var b = repository.GetItem(ID);
+
+            return View(b);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int ID, FAQViewModel FAQViewModel)
+        {
+            repository.Edit(FAQViewModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            repository.Delete(ID);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ BICT.Payetakht/Controllers/FAQController.cs
@@ -0,0 +1,22 @@
+﻿namespace BICT.Payetakht.Controllers
+{
+    using System.Web.Mvc;
+    using BICT.Payetakht.Data.Repository;
+
+    public class FAQController : Controller
+    {
+        private FAQRepository repository;
+
+        public FAQController()
+        {
+            repository = new FAQRepository();
+        }
+
+        public ActionResult Index(int p = 1)
+        {
+            ViewBag.Page = p;
+            var list = repository.GetPagedList(p, 5);
+            return View(list);
+        }
+    }
+}
