b83989e0416bc100e16b521831bd0c3ff611eca3
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-14 14:12:13 +0330

    picture of slider

---
+++ BICT.Payetakht.Data/Repository/PictureOfSlideRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BICT.Payetakht.Data.Models;
+using BICT.Payetakht.Data.ViewModels;
+
+namespace BICT.Payetakht.Data.Repository
+{
+    public class PictureOfSlideRepository : BaseRepository
+    {
+        public IList<PictureOfSlideViewModel> GetList()
+        {
+            return db.PictureOfSlides
+                .Select(x => new PictureOfSlideViewModel
+                {
+                    ID = x.ID,
+                    PicturePath = x.PicturePath,
+                })
+                .ToList();
+        }
+
+        public IList<PictureOfSlideViewModel> GetPagedList(int pageNum, int pageSize)
+        {
+            if (pageNum < 1)
+            {
+                pageNum = 1;
+            }
+            var skip = (pageNum - 1) * pageSize;
+            return db.PictureOfSlides
+                     .OrderByDescending(x => x.ID)
+                     .Skip(skip)
+                     .Take(pageSize)
+                     .Select(x => new PictureOfSlideViewModel
+                     {
+                         ID = x.ID,
+                         PicturePath = x.PicturePath,
+                     })
+                     .ToList();
+        }
+
+        public PictureOfSlideViewModel GetItem(int id)
+        {
+            return db.PictureOfSlides.Where(x => x.ID == id)
+                .Select(x => new PictureOfSlideViewModel
+                {
+                    ID = x.ID,
+                    PicturePath = x.PicturePath,
+                }).FirstOrDefault();
+        }
+
+        public void Create(string Path)
+        {
+            var item = new PictureOfSlide
+            {
+                PicturePath = Path,
+            };
+            db.PictureOfSlides.Add(item);
+            db.SaveChanges();
+        }
+
+        public string Delete(int ID)
+        {
+            var a = db.PictureOfSlides.Find(ID);
+            db.PictureOfSlides.Remove(a);
+            db.SaveChanges();
+            return a.PicturePath;
+        }
+    }
+}
---
+++ BICT.Payetakht.Data/ViewModels/PictureOfSlideViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+  public  class PictureOfSlideViewModel
+    {
+
+        public int ID { get; set; }
+
+
+        [Required(ErrorMessage = "افزودن عکس")]
+        [Display(Name = "عکاسخانه")]
+        public string PicturePath { get; set; }
+
+
+    }
+}
