d023feeeb9f6b3f305c8ddad62b48d2ca1498b7c
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-17 11:19:42 +0330

    HomeViewModel

---
+++ BICT.Payetakht.Data/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICT.Payetakht.Data.ViewModels
+{
+   public class HomeViewModel
+    {
+     public   IList<PictureOfSlideViewModel> Slide { get; set; }
+      public  IList<InspectionViewModel> Inspections { get; set; }
+    }
+}
