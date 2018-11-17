8e331452d17b4481f7f3ef914e9e90a24280c441
Mandana Hoseini <mandan.abolhosseini2@gmail.com>
2018-11-17 11:22:05 +0330

    ویو مدل صفحه اصلی

--- BICT.Payetakht/Controllers/HomeController.cs
+++ BICT.Payetakht/Controllers/HomeController.cs
@@ -1,23 +1,42 @@
 ﻿namespace BICT.Payetakht.Controllers
 {
+    using System.IO;
     using System.Web.Mvc;
     using BICT.Payetakht.Data.Repository;
+    using BICT.Payetakht.Data.ViewModels;
 
     public class HomeController : Controller
-
     {
-      private  PictureOfSlideRepository repository;
+        private PictureOfSlideRepository repository;
+        private AuditRepository auditrepository;
+
 
         public HomeController()
-
         {
             repository = new PictureOfSlideRepository();
+            auditrepository = new AuditRepository();
         }
 
         public ActionResult Index()
         {
-            var List = repository.GetList();
-            return View(List);
+            var x = new HomeViewModel
+            {
+                Inspections = auditrepository.GetLatestInspection(4),
+                Slide = repository.GetList()
+            };
+            foreach (var item in x.Inspections)
+            {
+                var path = Server.MapPath("~/Content/Audit/" + item.AuditID);
+                if (Directory.Exists(path))
+                {
+                    var files = Directory.GetFiles(path);
+                    if (files.Length > 0)
+                    {
+                        item.CoverPic = Path.GetFileName(files[0]);
+                    }
+                }
+            }
+            return View(x);
         }
 
         public ActionResult About()
